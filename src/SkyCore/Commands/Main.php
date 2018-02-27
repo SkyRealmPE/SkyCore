df6583f9471b977f50add0ede6b712a677d0a144
0pGam3rMC <kuasir321@gmail.com>
2018-02-26 23:54:51 -0500

    Update and rename src/Crafter20162017/Core/Commands to src/SkyCore/Commands/Main.php

---
+++ src/SkyCore/Commands/Main.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace SkyCore\Commands;
+
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+
+
+class Main extends PluginBase{
+
+  public function onEnable(){
+    $this-getLogger->info("SkyCore has been activated: Made by the Server Owner -> Crafter20162017");
+    $this-CoreCommands();
+  }
+  
+  public function CoreCommands(){
+  $this
+  }
+
+
+
+}
