f333c946da4a42cc3ee6023af1ccb7708f12e39e
0pGam3rMC <kuasir321@gmail.com>
2018-02-27 23:48:06 -0500

    Update Main.php

--- src/SkyCore/Loader/Main.php
+++ src/SkyCore/Loader/Main.php
@@ -1,1 +1,34 @@
+<?php
 
+namespace SkyCore\Loader
+  
+use pocketmine\Player;
+
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\utils\TextFormat as color;
+
+use SkyCore\Commands\Fly;
+
+class Main extends PluginBase {
+  
+  
+  public function onEnabled(){
+    $this-getLogger->info("SkyRealm's Core Plugin has been enabled!"):
+    $this-loadCore();
+  }
+  
+  public function onDisable(){
+    
+    
+  }
+  
+  public function loadCore(){
+    $this->getServer()->getCommandMap()->register("skyfly", new Fly("skyfly", $this));
+    
+  }
+  
+  
+  
+}
+  
