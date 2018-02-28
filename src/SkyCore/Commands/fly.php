673c5a09791c28c10bfee3925735df4ebe7f60ea
0pGam3rMC <kuasir321@gmail.com>
2018-02-27 23:24:01 -0500

    Update fly.php

--- src/SkyCore/Commands/fly.php
+++ src/SkyCore/Commands/fly.php
@@ -2,4 +2,58 @@
 
 namespace SkyCore\Commands;
 
-use 
+use pocketmine\Player;
+use pocketmine\entity\Entity;
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat as TF;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\event\entity\EntityDamageEvent;
+
+
+class Main extends PluginBase implements Listener{
+  
+  private $fly = [];
+    
+    public function onEnable(){
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getLogger()->info("Skyrealm's Fly Plugin enabled ");
+    }
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool{
+        if(strtolower($command->getName()) == "skyfly"){
+            if($sender->hasPermission("skycore.command") || $sender->isOp()){
+                if(isset($this->fly[strtolower($sender->getName())])){
+                    unset($this->fly[strtolower($sender->getName())]);
+                    $sender->setAllowFlight(false);
+                    $sender->setFlying(false);
+                    $sender->sendMessage(TF::RED."Fly has been disabled");
+                } else {
+                    $this->fly[strtolower($sender->getName())] = strtolower($sender->getName());
+                    $sender->setAllowFlight(true);
+                    $sender->setFlying(true);
+                    $sender->sendMessage(TF::GREEN."Fly has been enabled");
+                }
+            } else {
+                $sender->sendMessage(TF::RED."Sadly you dont have the permission to use /skyfly " . TF.GREEN . "You can donate at skyrealmpe.ddns.net for access!");
+            }
+        }
+        return true;
+    }
+    public function onHit(EntityDamageEvent $ev){
+        
+        if(($p = $ev->getEntity()) instanceof Player){
+            
+            if($ev->getCause() !== 4 && isset($this->fly[$p->getLowerCaseName()])){
+                $p->sendPopup(TF::RED . 'Fly has been disabled');
+                $p->setFlying(true);
+                $p->setAllowFlight(false);
+                unset($this->fly[$p->getLowerCaseName()]);
+            }
+        }
+    }
+}
+  
+  
+}
+
