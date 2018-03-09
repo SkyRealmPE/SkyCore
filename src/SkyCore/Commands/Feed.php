b841118ffd2a42cb6ca1de2b18dcd5d63ab53da2
0pGam3rMC <kuasir321@gmail.com>
2018-03-08 19:55:38 -0500

    Create Feed.php

---
+++ src/SkyCore/Commands/Feed.php
@@ -0,0 +1,75 @@
+<?php
+
+
+
+namespace SkyCore\Commands;
+
+use SkyCore\Loader\Main;
+
+use pocketmine\command\CommanderSender;
+
+use pocketmine\Player;
+
+class Feed extends BaseCommand {
+
+
+    private $plugin;
+    
+    public function __construct(Main $plugin) {
+    
+    $this->plugin = $plugin;
+    parent::__construct($plugin, "feed", "Access SkyRealm's /feed command", "/feed <user>", ["feed", "food"]);
+    
+    }
+    
+    public function execute(CommanderSender $sender, string $commandLabel, array $args) {
+    
+    if (!sender->hasPermission("skycore.command.feed"))  {
+        
+        $sender->sendMessage("Seriously? You know you cant use this command without having a donor rank: donate at skyrealmpe.buycraft.net");
+    
+    
+    }
+    
+    if ((!isset($args[0]) && !sender instanceof Player) || count($args) > 1) {
+    
+      $sender->sendMessage("You have to do /feed {player}. Please try again");
+      return true;
+    
+    }
+    
+    $player = $sender;
+    
+    if(isset($args[0]) && !($player = $this->getPlugin()->getServer()->getPlayer($args[0]))) {
+      
+      $sender->sendMessage("You didnt include a player when you use /feed");
+      
+      $return true;
+    
+    
+    
+    }
+    
+  if ($player->getName() !== $sender->getName() && !sender->hasPermission("skycore.command.feed.other"){
+  $sender->sendMessage("You have no permission to feed another"):
+    
+    $player->setFood(20);
+    $player->sendMessage("Successfully Fed");
+    
+    return true
+    
+    }
+    
+    
+    if ($player !== $sender) {
+      
+      $sender->sendMessage("Successfully Fed");
+    
+    }
+     return true;
+    
+    }
+    
+
+
+}
