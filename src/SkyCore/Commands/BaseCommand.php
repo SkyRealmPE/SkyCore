e19b781df3470213c0e533ab57de9321cbfb3e14
0pGam3rMC <kuasir321@gmail.com>
2018-03-08 19:23:14 -0500

    Create BaseCommand.php

---
+++ src/SkyCore/Commands/BaseCommand.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace SkyCore\Commands;
+
+use pocketmine\command\{Command, CommandSender, PluginIdentifiableCommand};
+
+class BaseCommand extends Command implements PluginIdentifiableCommand {
+
+    private $plugin;
+    
+    public function __construct(Loader $plugin
+
+
+
+
+
+
+}
