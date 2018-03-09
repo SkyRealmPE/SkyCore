ff77e2e95088b91cc0234d36dc5355c1ed64db19
0pGam3rMC <kuasir321@gmail.com>
2018-03-08 19:27:28 -0500

    Update BaseCommand.php

--- src/SkyCore/Commands/BaseCommand.php
+++ src/SkyCore/Commands/BaseCommand.php
@@ -4,15 +4,18 @@ namespace SkyCore\Commands;
 
 use pocketmine\command\{Command, CommandSender, PluginIdentifiableCommand};
 
+use SkyCore\Loader\Main;
+
 class BaseCommand extends Command implements PluginIdentifiableCommand {
 
     private $plugin;
     
-    public function __construct(Loader $plugin
-
-
-
-
-
-
+    public function __construct(Main $plugin, $name, $description, $usageMessage, $aliases) {
+        $this->plugin = $plugin;
+        parent::__construct($name, $description, $usageMessage, $aliases);
+    }
+    
+    public function getPlugin() : Plugin { 
+     return $this->plugin;   
+    }
 }
