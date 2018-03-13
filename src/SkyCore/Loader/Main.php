2cb5313f8ce30d4fa6b8659a971e0a69ada9c984
0pGam3rMC <kuasir321@gmail.com>
2018-03-13 09:34:37 -0400

    Update Main.php

--- src/SkyCore/Loader/Main.php
+++ src/SkyCore/Loader/Main.php
@@ -15,7 +15,7 @@ class Main extends PBASE{
 	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
 		switch($command->getName()){
 			case "skyfly":
-        if(!$sender instanceof Player){
+        if($sender instanceof Player){
           if($sender->hasPermission("skycore.command.fly")){
             #Fly code goes here
 				$sender->sendMessage("§b§lSkyFly §8>§r§b Fly has been enabled");
@@ -29,6 +29,19 @@ class Main extends PBASE{
           }
    }
     case "skyfeed":
+    if($sender instanceof Player)
+      if($sender->hasPermission("skycore.command.feed")){
+        #feed code goes here
+        $sender->sendMessage("§b§lSky§cFeed§8 >§r§b You have been fed");
+        return true;
+        
+        }else{
+        $sender->sendMessage("§b§lSky§cFeed§8 >§r§c You do not have permission to use the command §a/skyfeed§c. Please donate for a rank at §dskyrealmpe.buycraft.net");
+        return false;
+        }
+    
+      }
+    
     
     return true:
     
@@ -36,6 +49,6 @@ class Main extends PBASE{
     return false:
 	}
 	public function onDisable(){
-		$this->getLogger()->info("Bye");
+		$this->getLogger()->info("SkyRealmPE Core Plugin has been disabled");
 	}
 }
