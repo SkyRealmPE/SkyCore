a57de76f56a4e1eb710b33bad0bf927750357e5c
0pGam3rMC <kuasir321@gmail.com>
2018-03-13 16:49:43 -0400

    Update Main.php

--- src/SkyCore/Loader/Main.php
+++ src/SkyCore/Loader/Main.php
@@ -8,22 +8,41 @@ use pocketmine\command\PluginBase as Base;
 use pocketmine\Player;
 
 
-class Main extends PB {
+class Main extends Base {
        public function onEnable(){
 	       $this->getLogger->info("SkyRealm's Core has been enabled");
-						   }
+						  
+       }
        public function onDisable(){
 	       $this->getLogger->info("SkyRealm's Core has been disabled");
 			    
+       }
+	
+	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool {
+	    switch($command->getName()){
+			    
+		    case "skyfly":
+			    if($sender instanceof Player{
+			    if($sender->hasPermission("skycore.command.fly")){
+				    #fly code goes here
+				    $sender->sendMessage("SkyFly > you have successfull entered flight mode!");
+				    return true;
+			      }
+			    }else{
+				    $sender->sendMessage("SkyFly > you do not have permission");
+				    $sender->sendMessage("SkyFly > you need to buy a rank at skyrealmpe.buycraft.net or \n vote at bit.do/skyrealmpe");
+			            return false;
 			    }
+			       
+
+			    break;
+			   
+			    
+	    }#this line ends the cases(commands)
 	
-	
-	
-	       
-	       
-	       
-	
-	       }
+	      
+	}#this line the onCommand Function
+}#this line ends the plugin
 	
 	
 	
